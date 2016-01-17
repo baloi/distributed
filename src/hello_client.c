a82d69e4fcb8a7303f525003fe7b6079b3123b87
baloi <baloi.galia@gmail.com>
2016-01-17 13:56:38 -0500

    zero mq on my mac!

--- src/hello_client.c
+++ src/hello_client.c
@@ -8,7 +8,8 @@ int main(void)
     printf("Connecting to hello world server...\n");
     void *context = zmq_ctx_new();
     void *requester = zmq_socket(context, ZMQ_REQ);
-    zmq_connect(requester, "tcp://localhost:5555");
+    //zmq_connect(requester, "tcp://localhost:5555");
+    zmq_connect(requester, "tcp://192.168.0.14:5555");
 
     int request_nbr;
     for(request_nbr = 0; request_nbr != 10; request_nbr++)
@@ -17,7 +18,7 @@ int main(void)
         printf("Sending Hello %d...\n", request_nbr);
         zmq_send(requester, "Hello", 5, 0);        
         zmq_recv(requester, buffer, 10, 0);
-        printf("Received World %\n", request_nbr);
+        printf("Received World %d\n", request_nbr);
     }
     zmq_close(requester);
     zmq_ctx_destroy(context);
